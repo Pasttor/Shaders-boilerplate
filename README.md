ecbc20fbce0f879e995451a784751e3d4cd4df53
Pasttor <75398190+Pasttor@users.noreply.github.com>
2022-09-20 18:06:51 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-bshl53
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-bshl53)
---
+++ shaders/fragment.glsl
@@ -0,0 +1,10 @@
+uniform float time;
+
+varying float pulse;
+varying vec2 vUv;
+
+void main() {
+
+	gl_FragColor = vec4(vUv, 0., 1. );
+
+}
